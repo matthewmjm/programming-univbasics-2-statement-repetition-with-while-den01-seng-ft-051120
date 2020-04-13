fa537d436d55534d8f082e0a4ad4698c4acb2d21
Matthew Malecki <mmalecki4@gmail.com>
2020-04-13 20:58:15 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,9 @@
 # Write your code here
 
 countdown = 10
-while countdown > 1 do
+while countdown => 1 do
   puts "#{countdown}"
   break if countdown = 1
   countdown -= 1
 end  
-puts "Happy New Year!"
+#puts "Happy New Year!"
